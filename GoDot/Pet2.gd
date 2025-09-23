d67ea6037ff052d5370b73e24ab205e6e3a803f8
Cazandra Aporbo <83199148+Cazzy-Aporbo@users.noreply.github.com>
2025-09-22 22:33:47 -0700

    Create Pet2.gd

---
+++ GoDot/Pet2.gd
@@ -0,0 +1,70 @@
+extends Node
+
+# Stats
+var hunger = 80
+var happiness = 80
+var energy = 80
+var mischief = 20
+
+func _ready():
+    $Timer.connect("timeout", self, "_on_tick")
+    update_ui()
+    update_mood()
+
+func _on_tick():
+    # decay stats
+    hunger = max(hunger - 2, 0)
+    energy = max(energy - 1, 0)
+    mischief = min(mischief + 1, 100)
+    update_mood()
+    update_ui()
+
+func feed():
+    hunger = min(hunger + 25, 100)
+    happiness = min(happiness + 5, 100)
+    mischief = max(mischief - 5, 0)
+    play_animation("eat")
+    update_mood()
+
+func play():
+    happiness = min(happiness + 15, 100)
+    energy = max(energy - 10, 0)
+    mischief = min(mischief + 5, 100)
+    play_animation("play")
+    update_mood()
+
+func pet():
+    happiness = min(happiness + 10, 100)
+    mischief = max(mischief - 10, 0)
+    play_animation("happy")
+    update_mood()
+
+func update_ui():
+    $HungerBar.value = hunger
+    $HappinessBar.value = happiness
+    $EnergyBar.value = energy
+    $MischiefBar.value = mischief
+
+func update_mood():
+    if hunger < 30:
+        show_mood("sad")
+    elif happiness > 80 and energy > 50:
+        show_mood("playful")
+    elif mischief > 60:
+        show_mood("cheeky")
+    else:
+        show_mood("neutral")
+
+func show_mood(mood):
+    match mood:
+        "sad":
+            $MoodEmoji.texture = load("res://assets/emoji_sad.png")
+        "playful":
+            $MoodEmoji.texture = load("res://assets/emoji_playful.png")
+        "cheeky":
+            $MoodEmoji.texture = load("res://assets/emoji_cheeky.png")
+        "neutral":
+            $MoodEmoji.texture = load("res://assets/emoji_neutral.png")
+
+func play_animation(anim_name):
+    $PetSprite.play(anim_name)
