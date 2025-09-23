f8f036d3363d2a8a571ed672bb41108b218f20e7
Cazandra Aporbo <83199148+Cazzy-Aporbo@users.noreply.github.com>
2025-09-22 22:35:56 -0700

    Create game_manager.gd

---
+++ GoDot/game_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var collected_flowers = []
+
+func _ready():
+    for flower in $FlowerContainer.get_children():
+        flower.connect("collected", self, "_on_flower_collected")
+
+func _on_flower_collected(flower_name):
+    collected_flowers.append(flower_name)
+    print("Collected: ", flower_name)
+    update_journal_ui()
+
+func update_journal_ui():
+    var text = "Flowers Collected:\n"
+    for f in collected_flowers:
+        text += "- " + f + "\n"
+    $UI/JournalPanel.text = text
