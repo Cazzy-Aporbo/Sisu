00d9e29d517e6759a593b3cf47677ba4c62e4ff0
Cazandra Aporbo <83199148+Cazzy-Aporbo@users.noreply.github.com>
2025-09-22 22:30:46 -0700

    Create pet_buttons.gd

---
+++ GoDot/pet_buttons.gd
@@ -0,0 +1,11 @@
+# FeedButton signal
+func _on_FeedButton_pressed():
+    $Pet.feed()
+
+# PlayButton signal
+func _on_PlayButton_pressed():
+    $Pet.play()
+
+# SleepButton signal
+func _on_SleepButton_pressed():
+    $Pet.sleep()
